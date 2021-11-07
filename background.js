7574a45fb4d783d4da58fd9e95da924dbc0515e0
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2021-11-07 07:12:19 -0400

    Factoring...

--- background.js
+++ background.js
@@ -8,123 +8,180 @@ const contextMenu = () => {
         ]
     });
 };
+const botHosts = {remote: "https://api.contador.cloud", local: "https://localhost:8000"};
+let action = null;
 
-const actions = {
-    ecac_acesso_gov_certificate: {
-        dom: {
-            query: () => {
-                return document.title;
+function actions() {
+    // TODO: Load from github.
+    return {
+        ecac_acesso_gov_certificate: {
+            "cav.receita.fazenda.gov.br": {
+                onUserTrigger: {
+                    methods: {
+                        alert: ["Proximo Passo", "Clique no botão \"Entrar com gov.br\"."]
+                    }
+                }
+            },
+            "sso.acesso.gov.br": {
+                onPageLoad: {
+                    methods: {
+                        // () => document.querySelector('iframe[data-hcaptcha-response]').getAttribute('data-hcaptcha-response');
+                        dom: [() => {return document.title;}],
+                    },
+                    send: true
+                }
             }
-        },
-        cookies: {
-            domains: ["receita.fazenda.gov.br", "acesso.gov.br"],
-        },
-        redirect: true,
-    }
-};
+        }
+    };
+}
 
-// Runs only at extension instalation / refresh.
-let action = {};
-
-function alert(title, message) {
-    chrome.notifications.create("contadorCloudCookies", {
+function alert(title, message, interation = true) {
+    chrome.notifications.create("contadorCloudCookiesAlert", {
         type:'basic',
         iconUrl: "icon.png",
         message: message,
         title: title,
-        requireInteraction: true
+        requireInteraction: interation
     });
 }
 
-function dom(done) {
-    const query = action.dom.query;
-    const tabId = action.tab.id;
-    //const query = () => document.querySelector('iframe[data-hcaptcha-response]').getAttribute('data-hcaptcha-response');
-    chrome.scripting.executeScript({target: {tabId: tabId}, func: query}, queryResult => done(queryResult));
+function dom(queries, done) {
+    let results = {};
+    let tabId = action.current.tabId;
+    queries.forEach((query, index, queries) => {
+        chrome.scripting.executeScript({target: {tabId: tabId}, func: query}, result => {
+            results[index] = result;
+            isLastQuery = index === queries.length - 1;
+            if (isLastQuery)
+                done(results);
+        });
+    });
 }
 
 function cookies(done) {
-    const domains = action.cookies.domains;
+    const domains = action.domains;
     let jar = {};
     domains.forEach((domain, index, domains) => {
         chrome.cookies.getAll({ domain: domain }, cookies => {
             jar[domain] = cookies;
             isLastDomain = index === domains.length - 1;
-            if (isLastDomain)
+            if (isLastDomain) {
+                empty(jar);
                 done(jar);
+            }
         });
     });
 }
 
-function send(body) {
-    const tabId = action.tab.id;
-    console.log(body);
-    console.log(action);
-
-    /*fetch(apiHost + '/' + endPoint, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(body),
-    })
-    .then(response => response.json())
-    .then(data => {
-        this.postData = data;
-    })
-    .catch((error) => {
-        console.log('Error:', error);
-    });*/
-
-    action = {};
-    //chrome.tabs.update(tabId, {"url": apiHost});
-}
-
-function remove(cookies) {
-    console.log(cookies);
-    console.log("from remove");
+function empty(jar) {
+    console.log(jar);
 
     //chrome.cookies.remove({url: "https://cav.receita.fazenda.gov.br", name: "ASP.NET_SessionId"});
     //chrome.cookies.remove({url: "https://cav.receita.fazenda.gov.br/autenticacao/login/govbrsso", name: "ECAC_NONCE_GOVBR"});
 }
 
-function execute() {
-    const hasAction = JSON.stringify(action) !== JSON.stringify({});
+function redirect(to, tabId) {
+    action = null;
+    chrome.tabs.update(tabId, {"url": to});
+}
 
-    if (!hasAction) // TODO: issue a notification when triggered by the user.
-        return;
+function send(body) {
+    console.log(body);
+    /*fetch(apiHost + '/' + endPoint, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body),
+    })
+    //.then(response => response.json())
+    //.then(data => {})
+    .catch((error) => {
+        console.log("Error:", error);
+        alert("Error:", error);
+    });*/
 
-    const hasDom = action.hasOwnProperty('dom');
-    if (hasDom) {
-        dom(items => {
-            cookies(jar => {
-                send({dom: items, cookies: jar});
-            });
-        });
+    //redirect(action.redirectTo, action.current.tabId);
+}
+
+function run(triggeredBy) {
+    const hasAction = action;
+    const hasDom = action?.current?.triggers[triggeredBy]?.methods?.dom;
+    const hasAlert = action?.current?.triggers[triggeredBy]?.methods?.alert;
+    const goAhead = action?.current?.triggers[triggeredBy]?.send;
+    const sendToBot = (items = false) => cookies(jar => send({dom: items, cookies: jar, urls: action.urls}));
+
+    if (!hasAction) {
+        alert("Contado.Cloud", "Nada a ser feito.");
         return;
     }
-    cookies(jar => {
-        send({dom: false, cookies: jar});
-    });
+
+    if (message = hasAlert) {
+        alert(...message);
+
+        if (!goAhead) return;
+    }
+
+    if (queries = hasDom)
+        return dom(queries, items => sendToBot(items));
+
+    sendToBot();
 }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
-        //const redirect = (action.hasOwnProperty('redirect') ? action.redirect : false);
+    if (changeInfo.status !== 'complete') return;
 
-        chrome.cookies.get({ url: "https://abc.net.au", name: "ABC_LD" }, cookie => {
-            action = actions["ecac_acesso_gov_certificate"];
-            action.tab = {id: tabId, url: tab.url};
+    const url = new URL(tab.url);
+    const bot = JSON.parse(url.searchParams.get("contadorCloud"));
+    const sendToBot = () => action?.current?.triggers?.onPageLoad?.send;
+    const hasAction = () => {
+        if (bot) {
+            action = {
+                name: bot.action,
+                current: {triggers: actions()[bot.action][url.host]},
+                redirectTo: botHosts[bot.host] + "/" + bot.uri,
+                domains: Object.keys(actions()[bot.action]),
+                urls: {[url.host]: [tab.url]},
+            };
+            return true;
+        }
 
-        });
-    }
-})
+        if (action) {
+            if (!actions()[action.name].hasOwnProperty(url.host)) {
+                alert("Atividade NÃO corresponde ao website " + url.host, "Caso o problema persista, entre em contado conosco ( Contador.Cloud ).");
+                redirect(action.redirectTo, tabId);
+                return false;
+            }
 
-chrome.action.onClicked.addListener(tab => {
-    execute();
+            action.current = {triggers: actions()[action.name][url.host]};
+            (() => {
+                if (action.urls.hasOwnProperty(url.host))
+                    return action.urls[url.host].push(tab.url);
+
+                action.urls[url.host] = [tab.url];
+            })();
+            return true;
+        }
+        return false;
+    };
+
+    if (hasAction())
+        action.current.tabId = tabId;
+
+    if(sendToBot())
+        run("onPageLoad");
 });
 
-chrome.contextMenus.onClicked.addListener(((info, tab) => {
-    execute();
-}));
-
+/**
+ * User action triggered via extension menu located at
+ * the right end of the address bar.
+ */
+chrome.action.onClicked.addListener(tab => run("onUserTrigger"));
+/**
+ * User action triggered when right clicking on the web page.
+ */
+chrome.contextMenus.onClicked.addListener((info, tab) => run("onUserTrigger"));
+/**
+ * Inserts this module into the pop up list available when right
+ * clicking on the web page.
+ */
 chrome.runtime.onStartup.addListener(contextMenu);
 chrome.runtime.onInstalled.addListener(contextMenu);
